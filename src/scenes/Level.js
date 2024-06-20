1c22191760a061138e8848e1538b937dc809d336
weihai.tang <weihai.tang@akila3d.com>
2024-06-20 23:24:25 +0800

    update

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -21,23 +21,34 @@ class Level extends Phaser.Scene {
 		sky.setOrigin(0, 0);
 
 		// platform
-		const platform = this.add.sprite(0, 538, "platform");
+		const platform = this.physics.add.image(0, 538, "platform");
 		platform.scaleX = 2;
 		platform.scaleY = 2;
 		platform.setOrigin(0, 0);
+		platform.body.setSize(400, 32, false);
 
 		// platform_1
-		this.add.sprite(66, 325, "platform");
+		const platform_1 = this.physics.add.image(66, 325, "platform");
+		platform_1.body.setSize(400, 32, false);
 
 		// platform_2
-		this.add.sprite(598, 427, "platform");
+		const platform_2 = this.physics.add.image(598, 427, "platform");
+		platform_2.body.setSize(400, 32, false);
 
 		// platform_3
-		this.add.sprite(713, 200, "platform");
+		const platform_3 = this.physics.add.image(713, 200, "platform");
+		platform_3.body.setSize(400, 32, false);
 
-		// turn
-		const turn = this.add.sprite(327, 433, "dude", 4);
-		turn.play("turn");
+		// player
+		const player = this.physics.add.sprite(327, 433, "dude", 4);
+		player.setInteractive(new Phaser.Geom.Rectangle(0, 0, 32, 48), Phaser.Geom.Rectangle.Contains);
+		player.body.velocity.x = 200;
+		player.body.velocity.y = 200;
+		player.body.gravity.x = 200;
+		player.body.gravity.y = 200;
+		player.body.bounce.x = 0.2;
+		player.body.allowGravity = false;
+		player.body.setSize(33, 50, false);
 
 		// left
 		const left = this.add.sprite(195, 417, "dude", 0);
@@ -46,6 +57,9 @@ class Level extends Phaser.Scene {
 		// right
 		const right = this.add.sprite(377, 290, "dude", 5);
 		right.play("right");
+
+		// collider
+		this.physics.add.collider();
 
 		this.events.emit("scene-awake");
 	}
